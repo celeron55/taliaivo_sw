e22ba8d122154322ca7b0b79c8c75a473e6f71f4
Perttu Ahola <celeron55@gmail.com>
2023-11-21 08:55:41 +0200

    WIP

--- sumobrain_common/src/map.rs
+++ sumobrain_common/src/map.rs
@@ -275,15 +275,17 @@ impl HoughLine {
     }
     pub fn vector_to_point(&self, point: Vector2<f32>) -> Vector2<f32> {
         let angle_rad = self.angle.to_radians();
-        let self_normal = Vector2::new(angle_rad.cos(), angle_rad.sin());
-        let rho = self.distance;
+        let line_direction = Vector2::new(angle_rad.cos(), angle_rad.sin());
+        let line_normal = Vector2::new(-line_direction.y, line_direction.x);
+        let line_point = line_direction * self.distance;
 
-        // Project the point onto the self
-        let projected_length = point.dot(&self_normal);
-        let closest_point_on_self = self_normal * projected_length;
+        // Project the point onto the line normal
+        let point_to_line = line_point - point;
+        let projected_length = point_to_line.dot(&line_normal);
+        let closest_point_on_line = point + line_normal * projected_length;
 
-        // The vector from the closest point on the self to the point
-        point - closest_point_on_self
+        // The vector from the point to the closest point on the line
+        closest_point_on_line - point
     }
 }
 
