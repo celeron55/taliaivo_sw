e06c393af8735bba2c0068f2ab62c68c35f84845
Perttu Ahola <celeron55@gmail.com>
2023-12-09 21:21:06 +0200

    sumobrain_embedded: Remove peripheral access from panic handler because it wouldn't work anyway after peripherals have been accessed in main()

--- sumobrain_embedded/src/main.rs
+++ sumobrain_embedded/src/main.rs
@@ -14,28 +14,6 @@ use sumobrain_common::map::HoughLine;
 
 #[panic_handler]
 fn panic(panic_info: &core::panic::PanicInfo) -> ! {
-    if let (Some(dp), Some(cp)) = (
-        pac::Peripherals::take(),
-        cortex_m::peripheral::Peripherals::take(),
-    ) {
-        // Set up the LED
-        let gpioa = dp.GPIOA.split();
-        let mut led = gpioa.pa8.into_push_pull_output();
-
-        // Set up the system clock. We want to run at 48MHz for this one.
-		// TODO: 168MHz
-        let rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
-
-        // Create a delay abstraction based on SysTick
-        let mut delay = cp.SYST.delay(&clocks);
-
-        loop {
-            // On for 1s, off for 1s.
-            led.toggle();
-            delay.delay_ms(100_u32);
-        }
-    }
     loop {}
 }
 
