dd99ee0167dc0cd95d0b200b4b2ee4a499125d55
Perttu Ahola <celeron55@gmail.com>
2023-12-09 19:59:29 +0200

    sumobrain_embedded: LED blink works on real hardware

--- sumobrain_embedded/build_release.sh
+++ sumobrain_embedded/build_release.sh
@@ -2,4 +2,9 @@
 set -euv
 dir="$( cd "$( dirname "$0" )" && pwd )"
 cd "$dir"
+
 cargo build --target thumbv7em-none-eabihf --release $@
+
+# Target is in ELF format. Convert to binary
+arm-none-eabi-objcopy -O binary ../target/thumbv7em-none-eabihf/release/sumobrain_embedded ../target/thumbv7em-none-eabihf/release/sumobrain_embedded.bin
+
--- sumobrain_embedded/src/main.rs
+++ sumobrain_embedded/src/main.rs
@@ -11,17 +11,19 @@ use stm32f4xx_hal as hal;
 
 use crate::hal::{pac, prelude::*};
 
-#[entry]
+//#[entry]
+#[cortex_m_rt::entry]
 fn main() -> ! {
     if let (Some(dp), Some(cp)) = (
         pac::Peripherals::take(),
         cortex_m::peripheral::Peripherals::take(),
     ) {
-        // Set up the LED. On the Nucleo-446RE it's connected to pin PA5.
+        // Set up the LED
         let gpioa = dp.GPIOA.split();
-        let mut led = gpioa.pa5.into_push_pull_output();
+        let mut led = gpioa.pa8.into_push_pull_output();
 
         // Set up the system clock. We want to run at 48MHz for this one.
+		// TODO: 168MHz
         let rcc = dp.RCC.constrain();
         let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
 
