b22015cb30bf29fb0a1f4d89d8097220b35388fa
Perttu Ahola <celeron55@gmail.com>
2024-01-27 00:51:03 +0200

    WIP: Try to get RTIC to work

--- sumobrain_embedded/build_release.sh
+++ sumobrain_embedded/build_release.sh
@@ -4,7 +4,7 @@ dir="$( cd "$( dirname "$0" )" && pwd )"
 cd "$dir"
 
 # Target is set in .cargo/config.toml
-cargo build --release $@
+cargo +nightly build --release $@
 
 # Target is in ELF format. Convert to binary
 arm-none-eabi-objcopy -O binary ../target/thumbv7em-none-eabihf/release/sumobrain_embedded ../target/thumbv7em-none-eabihf/release/sumobrain_embedded.bin
--- sumobrain_embedded/src/main.rs
+++ sumobrain_embedded/src/main.rs
@@ -4,8 +4,12 @@
 //#![deny(unsafe_code)]
 //#![allow(clippy::empty_loop)]
 
-use sumobrain_common::{RobotInterface, BrainState, Map};
-use sumobrain_common::map::HoughLine;
+//use cortex_m_rt::{entry, exception};
+//use cortex_m::interrupt::{Mutex, CriticalSection};
+use stm32f4xx_hal::gpio::{Output, PushPull, PA8};
+use stm32f4xx_hal::prelude::*;
+use rtic::app;
+use rtic_monotonics::systick::*;
 
 use arrayvec::{ArrayVec, ArrayString};
 use nalgebra::{Vector2, Point2, Rotation2};
@@ -15,24 +19,6 @@ use core::fmt::Write; // For ArrayString
 use core::borrow::BorrowMut;
 use core::sync::atomic::{Ordering, AtomicU32, AtomicBool};
 use static_cell::StaticCell;
-
-use cortex_m_rt::{entry, exception};
-use cortex_m::interrupt::{Mutex, CriticalSection};
-use embassy_stm32::Peripherals;
-use embassy_executor::{Spawner, Executor, InterruptExecutor};
-use embassy_stm32::interrupt;
-use embassy_stm32::interrupt::{InterruptExt, Priority};
-use embassy_stm32::exti::ExtiInput;
-use embassy_stm32::gpio::{AnyPin, Input, Output, Pin, Pull, Speed};
-use embassy_stm32::gpio;
-use embassy_stm32::time::Hertz;
-use embassy_stm32::usb_otg::{Driver, Instance};
-use embassy_stm32::{bind_interrupts, peripherals, usb_otg, Config};
-use embassy_time::{Duration, Timer, Instant, Ticker};
-use embassy_usb::Builder;
-use embassy_usb::driver::EndpointError;
-use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
-use embassy_usb::class::cdc_acm;
 use futures::future::join;
 use futures::join;
 use log;
@@ -41,17 +27,8 @@ use once_cell::race::OnceRef;
 //use defmt::{panic, *};
 //use {defmt_rtt as _, panic_probe as _};
 
-static WANTED_LED_STATE: AtomicBool = AtomicBool::new(true);
-static USB_LOGGING_ENABLED: AtomicBool = AtomicBool::new(false);
-
-#[panic_handler]
-fn panic(panic_info: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
-
-bind_interrupts!(struct Irqs {
-    OTG_FS => usb_otg::InterruptHandler<peripherals::USB_OTG_FS>;
-});
+use sumobrain_common::{RobotInterface, BrainState, Map};
+use sumobrain_common::map::HoughLine;
 
 struct Robot {
     wheel_speed_left: f32,
@@ -139,275 +116,161 @@ impl RobotInterface for Robot {
     }
 }
 
-struct UsbLogger {
-    buffer: Mutex<RefCell<Option<ArrayString<1024>>>>,
-}
+#[app(device = stm32f4xx_hal::pac, peripherals = true, dispatchers = [UART4, UART5, USART6])]
+mod app {
+    use super::*;
 
-impl UsbLogger {
-    async fn flush_to_usb<'d, T: embassy_stm32::usb_otg::Instance>(
-            &self, sender: &mut cdc_acm::Sender<'d, Driver<'d, T>>)
-            -> Result<(), EndpointError> {
-        let mut buf2: Option<ArrayString<1024>> = Some(ArrayString::new());
-        cortex_m::interrupt::free(|cs| {
-            // This replaces the logger buffer with an empty one, and we get the
-            // possibly filled in one
-            buf2 = self.buffer.borrow(cs).replace(buf2);
-        });
-        if let Some(ref mut buffer) = buf2 {
-            if !buffer.is_empty() && USB_LOGGING_ENABLED.load(Ordering::Relaxed) {
-                // Toggle LED
-                //WANTED_LED_STATE.fetch_xor(true, Ordering::Relaxed); // Toggle
-                // Write the buffer contents to the USB CDC and clear the buffer
-                sender.write_packet(buffer.as_bytes()).await?;
-            }
-        }
-        Ok(())
-    }
-}
-
-impl Log for UsbLogger {
-    fn enabled(&self, metadata: &Metadata) -> bool {
-        metadata.level() <= log::Level::Info // Adjust as needed
+    #[shared]
+    struct Shared {
+        //wanted_led_state: Mutex<RefCell<bool>>,
+        //wanted_led_state: AtomicBool,
+        wanted_led_state: bool,
     }
 
-    fn log(&self, record: &Record) {
-        if self.enabled(record.metadata()) {
-            cortex_m::interrupt::free(|cs| { 
-                if let Some(ref mut buffer) = self.buffer.borrow(cs).borrow_mut().deref_mut() {
-                    let _ = writeln!(buffer, "[{}] {}\r", record.level(), record.args());
-                }
+    #[local]
+    struct Local {
+        led_pin: PA8<Output<PushPull>>,
+        state: bool, // TODO: Use or remove
+    }
+
+    #[init]
+    fn init(cx: init::Context) -> (Shared, Local) {
+        // System clock
+
+        /* TODO: This Embassy config seemed to closest match what we actually
+                 want. Try to get the same result here
+    
+        let mut config = Config::default();
+        {
+            use embassy_stm32::rcc::*;
+            config.rcc.hse = Some(Hse {
+                freq: Hertz(16_000_000),
+                mode: HseMode::Oscillator,
             });
+            config.rcc.pll_src = PllSource::HSE;
+            config.rcc.pll = Some(Pll {
+                prediv: PllPreDiv::DIV8,
+                mul: PllMul::MUL168,
+                divp: Some(PllPDiv::DIV2), // 8mhz / 4 * 168 / 2 = 168Mhz.
+                divq: Some(PllQDiv::DIV7), // 8mhz / 4 * 168 / 7 = 48Mhz.
+                divr: None,
+            });
+            config.rcc.ahb_pre = AHBPrescaler::DIV1;
+            config.rcc.apb1_pre = APBPrescaler::DIV4;
+            config.rcc.apb2_pre = APBPrescaler::DIV2;
+            config.rcc.sys = Sysclk::PLL1_P;
         }
-    }
+        let p = embassy_stm32::init(config);
+        */
 
-    fn flush(&self) {
-        // Flushing is handled elsewhere
-    }
-}
+        let rcc = cx.device.RCC.constrain();
+        //let clocks = rcc.cfgr.sysclk(48.MHz()).freeze(); // Internal at 48MHz
+        //let clocks = rcc.cfgr.sysclk(168.MHz()).freeze();
+        // Results in about 39mA taken via USB with stlink not connected
+        // - Powering via stlink adds about 34mA
+        let clocks = rcc.cfgr
+            .use_hse(16.MHz()) // Use external crystal (HSE)
+            .hclk(168.MHz())
+            .pclk1(42.MHz())
+            .pclk2(84.MHz())
+            .sysclk(168.MHz()) // Set system clock (SYSCLK)
+            .freeze(); // Apply the configuration
 
-static LOGGER: UsbLogger = UsbLogger {
-    buffer: Mutex::new(RefCell::new(None)),
-};
+        // SysTick
 
-// Function to initialize the logger
-fn init_logger() {
-    cortex_m::interrupt::free(|cs| {
-        LOGGER.buffer.borrow(cs).replace(Some(ArrayString::new()));
-    });
-    log::set_logger(&LOGGER).unwrap();
-    log::set_max_level(log::LevelFilter::Info); // TODO: Adjust
-}
+        let systick_mono_token = rtic_monotonics::create_systick_token!();
+        let systick_interval_ms = 1;
+        // No idea what's going on, for some reason the value needs to be divided by
+        // 8 in order to get correct timing (TODO, FIXME)
+        Systick::start(cx.core.SYST,
+                clocks.sysclk().to_Hz() / 1000 * systick_interval_ms / 8 - 1,
+                systick_mono_token);
 
-async fn flush_log_to_usb<'d, T: Instance + 'd>(sender: &mut cdc_acm::Sender<'d, Driver<'d, T>>)
-        -> Result<(), EndpointError> {
-    loop {
-        LOGGER.flush_to_usb(sender).await?;
-        // TODO: Adjust delay
-        Timer::after_millis(20).await;
-    }
-}
+        // I/O
 
-static EXECUTOR_HIGH: InterruptExecutor = InterruptExecutor::new();
-static EXECUTOR_LOW: StaticCell<Executor> = StaticCell::new();
+        let gpioa = cx.device.GPIOA.split();
 
-#[interrupt]
-unsafe fn UART4() {
-    EXECUTOR_HIGH.on_interrupt()
-}
+        let mut led_pin = gpioa.pa8.into_push_pull_output();
+        led_pin.set_high();
 
-// TODO: Figure out how to make this work without a global static
-static mut EP_OUT_BUFFER: [u8; 256] = [0u8; 256];
-
-#[embassy_executor::main]
-async fn main(spawner: Spawner) {
-    // System clock
-
-    let mut config = Config::default();
-    {
-        use embassy_stm32::rcc::*;
-        config.rcc.hse = Some(Hse {
-            freq: Hertz(16_000_000),
-            mode: HseMode::Oscillator,
-        });
-        config.rcc.pll_src = PllSource::HSE;
-        config.rcc.pll = Some(Pll {
-            prediv: PllPreDiv::DIV8,
-            mul: PllMul::MUL168,
-            divp: Some(PllPDiv::DIV2), // 8mhz / 4 * 168 / 2 = 168Mhz.
-            divq: Some(PllQDiv::DIV7), // 8mhz / 4 * 168 / 7 = 48Mhz.
-            divr: None,
-        });
-        config.rcc.ahb_pre = AHBPrescaler::DIV1;
-        config.rcc.apb1_pre = APBPrescaler::DIV4;
-        config.rcc.apb2_pre = APBPrescaler::DIV2;
-        config.rcc.sys = Sysclk::PLL1_P;
-    }
-    let p = embassy_stm32::init(config);
-
-    // Utilities
-
-    init_logger();
-
-    // I/O
-
-    /*let gpioa = dp.GPIOA.split();
-    let mut led_pin = gpioa.pa8.into_push_pull_output();
-    led_pin.set_high();
-    let mut debug_pin = dp.GPIOB.split().pb10.into_push_pull_output();*/
-    /*cortex_m::interrupt::free(|cs| {
-        LED_PIN.borrow(cs).replace(Some(led_pin));
-        DEBUG_PIN.borrow(cs).replace(Some(debug_pin));
-    });*/
-
-    // USB
-
-    // Create the driver, from the HAL.
-    let mut config = embassy_stm32::usb_otg::Config::default();
-    // NOTE: For this, VBUS would have to be connected to PA9
-    config.vbus_detection = true;
-    //config.vbus_detection = false;
-    let driver = unsafe {
-        // TODO: Figure out how to make this work without unsafe
-        Driver::new_fs(p.USB_OTG_FS, Irqs, p.PA12, p.PA11, &mut EP_OUT_BUFFER, config)
-    };
-
-    // Executors and tasks
-
-    // High-priority executor: UART4, priority level 6
-    interrupt::UART4.set_priority(Priority::P6);
-    let spawner = EXECUTOR_HIGH.start(interrupt::UART4);
-    spawner.spawn(led_task(p.PA8.degrade())).unwrap();
-    spawner.spawn(usb_task(driver)).unwrap();
-
-    // Low priority executor: runs in thread mode, using WFE/SEV
-    let executor = EXECUTOR_LOW.init(Executor::new());
-    executor.run(|spawner| {
-        spawner.spawn(background_processing_task()).unwrap();
-    }); 
-}
-
-#[embassy_executor::task]
-async fn usb_task(mut driver: Driver<'static, embassy_stm32::peripherals::USB_OTG_FS>) {
-
-    // Create embassy-usb Config
-    let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
-    config.manufacturer = Some("8Dromeda Productions");
-    config.product = Some("Taliaivo");
-    config.serial_number = Some("1337");
-
-    // Required for windows compatibility.
-    // https://developer.nordicsemi.com/nRF_Connect_SDK/doc/1.9.1/kconfig/CONFIG_CDC_ACM_IAD.html#help
-    config.device_class = 0xEF;
-    config.device_sub_class = 0x02;
-    config.device_protocol = 0x01;
-    config.composite_with_iads = true;
-
-    // Create embassy-usb DeviceBuilder using the driver and config.
-    // It needs some buffers for building the descriptors.
-    let mut device_descriptor = [0; 256];
-    let mut config_descriptor = [0; 256];
-    let mut bos_descriptor = [0; 256];
-    let mut control_buf = [0; 64];
-
-    let mut state = State::new();
-
-    let mut builder = Builder::new(
-        driver,
-        config,
-        &mut device_descriptor,
-        &mut config_descriptor,
-        &mut bos_descriptor,
-        &mut [], // no msos descriptors
-        &mut control_buf,
-    );
-
-    // Create classes on the builder.
-    let mut acm = CdcAcmClass::new(&mut builder, &mut state, 64);
-
-    let (mut sender, mut receiver) = acm.split();
-    //let (mut sender, mut receiver, mut control) = acm.split_with_control();
-
-    // Build the builder.
-    let mut usb = builder.build();
-
-    // Run the USB device.
-    let usb_fut = usb.run();
-
-    // Handle the ACM class
-    let acm_sender_fut = async {
+        /*// Dumb synchronous busywait led blink for testing
         loop {
-            sender.wait_connection().await;
-            let flusher = flush_log_to_usb(&mut sender);
-            let input_handler = usb_serial_input_handler(&mut receiver);
-            let (flusher_result, input_handler_result) = join!(flusher, input_handler);
-            match flusher_result {
-                Ok(_) => (),
-                Err(_) => loop {
-                    // TODO: Instead of this loop, reset USB in such a way that
-                    //       a new connection can be made
-                    USB_LOGGING_ENABLED.store(false, Ordering::Relaxed);
-                    WANTED_LED_STATE.store(true, Ordering::Relaxed);
-                    Timer::after_millis(125).await;
-                    WANTED_LED_STATE.store(false, Ordering::Relaxed);
-                    Timer::after_millis(125).await;
-                },
-            };
-        }
-    };
-
-    // Run everything concurrently.
-    // If we had made everything `'static` above instead, we could do this using
-    // separate tasks instead.
-    join!(usb_fut, acm_sender_fut);
-}
-
-#[embassy_executor::task]
-async fn background_processing_task() {
-    let mut brain = BrainState::new(0);
-    let mut robot: Robot = Robot::new();
-
-    let interval_us = 1000000 / sumobrain_common::UPS as u64;
-    let mut ticker = Ticker::every(Duration::from_micros(interval_us));
-    loop {
-        brain.update(&mut robot);
-
-        //cortex_m::asm::delay(16_000_000);
-        //robot.wheel_speed_right += 1.0;
-
-        info!("wheel_speed: {:?} {:?}", robot.wheel_speed_left, robot.wheel_speed_right);
-
-        // Make sure other tasks get at least some time
-        //Timer::after_millis(1).await;
-
-        ticker.next().await;
-
-        WANTED_LED_STATE.fetch_xor(true, Ordering::Relaxed); // Toggle LED
-    }
-}
-
-#[embassy_executor::task]
-async fn led_task(pin: AnyPin) {
-    let mut led = Output::new(pin, gpio::Level::Low, Speed::Low);
-
-    loop {
-        let wanted_state = WANTED_LED_STATE.load(Ordering::Relaxed);
-        led.set_level(wanted_state.into());
-        Timer::after_millis(10).await;
-    }
-}
-
-async fn usb_serial_input_handler<'d, T: Instance + 'd>(
-        receiver: &mut cdc_acm::Receiver<'d, Driver<'d, T>>) -> Result<(), EndpointError> {
-    let mut buf = [0; 64];
-    loop {
-        let n = receiver.read_packet(&mut buf).await?;
-        let data = &buf[..n];
-        info!("data: {:?}", data);
-        // Any received data starts USB logging
-        USB_LOGGING_ENABLED.store(true, Ordering::Relaxed);
-        // Some commands could be handled like this, altough buffering should be
-        // added
-        /*if data.len() >= 1 && data[0] == b'a' {
+            led_pin.set_high();
+            cortex_m::asm::delay(16_000_000);
+            led_pin.set_low();
+            cortex_m::asm::delay(16_000_000);
         }*/
+
+        let mut debug_pin = cx.device.GPIOB.split().pb10.into_push_pull_output();
+
+        // Schedule tasks
+
+        led_task::spawn().ok();
+
+        // Initialize context
+
+        (
+            Shared {
+                //wanted_led_state: Mutex::new(RefCell::new(true)),
+                //wanted_led_state: AtomicBool::new(true),
+                wanted_led_state: true,
+            },
+            Local {
+                led_pin: led_pin,
+                state: false,
+            }
+        )
     }
+
+    /*#[idle(shared = [wanted_led_state], local = [])]
+    fn idle(mut cx: idle::Context) -> ! {
+        loop {
+            // TODO: Enable
+            /*let mut brain = BrainState::new(0);
+            let mut robot: Robot = Robot::new();
+
+            let interval_us = 1000000 / sumobrain_common::UPS as u64;
+            // TODO: Interval timing
+            //let mut ticker = Ticker::every(Duration::from_micros(interval_us));
+            loop {
+                brain.update(&mut robot);
+
+                //cortex_m::asm::delay(16_000_000);
+                //robot.wheel_speed_right += 1.0;
+
+                info!("wheel_speed: {:?} {:?}", robot.wheel_speed_left, robot.wheel_speed_right);
+
+                // Make sure other tasks get at least some time
+                //Timer::after_millis(1).await;
+
+                //ticker.next().await;
+
+                // Toggle LED for debugging
+                cx.shared.wanted_led_state.lock(|value| { *value = !*value; });
+            }*/
+        }
+    }*/
+
+    //#[task(priority = 1, shared = [wanted_led_state], local = [led_pin, state])]
+    #[task(shared = [wanted_led_state], local = [led_pin, state])]
+    async fn led_task(mut cx: led_task::Context) {
+        loop {
+            /*let wanted_state = cx.shared.wanted_led_state.lock(|value| {
+                *value
+            });
+            cx.local.led_pin.set_state(wanted_state.into());
+            Systick::delay(10.millis()).await;*/
+
+            *cx.local.state = !*cx.local.state;
+            let wanted_state = *cx.local.state;
+            cx.local.led_pin.set_state(wanted_state.into());
+            //cortex_m::asm::delay(16_000_000); // Busywait
+            Systick::delay(500.millis()).await;
+        }
+    }
+}
+
+#[panic_handler]
+fn panic(panic_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
