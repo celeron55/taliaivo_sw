45b19d01c3cd0054d661bc502bdc433454fcff8a
Perttu Ahola <celeron55@gmail.com>
2024-03-24 13:21:29 +0200

    embedded: Fix sensor order for new body

--- taliaivo_embedded/src/main.rs
+++ taliaivo_embedded/src/main.rs
@@ -86,8 +86,8 @@ const PROXIMITY_SENSOR_COUNT: usize = 6;
 const PROXIMITY_SENSOR_ANGLES: [f32; PROXIMITY_SENSOR_COUNT] =
         [0.0, -45.0, 45.0, -90.0, 90.0, 180.0];
 const PROXIMITY_SENSOR_ADCS: [usize; PROXIMITY_SENSOR_COUNT] =
-        [2, 3, 1, 4, 0, 5]; // (use this)
-        //[2, 2, 2, 4, 0, 5]; // Disable -45 and +45 (TODO: don't use this)
+        [1, 2, 0, 3, 4, 5];
+      //[2, 3, 1, 4, 0, 5]; // TODO: Remove: Order in old body
 
 const SERVO_DUTY_CLOCKS_MIN: u16 = 27800;
 const SERVO_DUTY_CLOCKS_MAX: u16 = 53200;
